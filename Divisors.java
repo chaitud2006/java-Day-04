5e38cd95c2bc22cef65eeb147620f699648ba6a4
chaitud2006 <cc2214574@gmail.com>
2024-12-05 12:40:58 +0530

    Add files via upload

---
+++ Divisors.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Divisors
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<=n;i+=3)
+        {
+            System.out.println(i);
+        }
+    }
+}
