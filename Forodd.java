821b428d97eb918b8b1966a6663944aeb6dd4ad7
chaitud2006 <cc2214574@gmail.com>
2024-12-05 12:33:58 +0530

    Add files via upload

---
+++ Forodd.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Forodd
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=10;i+=2)
+        {
+            System.out.println(i);
+        }
+    }
+}
